24b9ae7af1b65e124048afaa0d4a72b14e4e8d39
BentSpace <bentspacetime@gmail.com>
2022-03-26 14:01:51 -0700

    Added arg check

--- batchSchedulingComparison.py
+++ batchSchedulingComparison.py
@@ -2,9 +2,13 @@ import sys
 
 def main():
   print("Hello World!")
-  print ('Number of arguments:', len(sys.argv), 'arguments.')
-  print ('Argument List:', str(sys.argv))
-
+  print('Number of arguments:', len(sys.argv), 'arguments.')
+  print('Argument List:', str(sys.argv))
+  if (len(sys.argv) != 3):
+    print("\nMust use with 3 arguements. \n" +\
+    "File name, batchfile name, and scheduling algorithm. \n" +\
+    "Please try again.\n")
+    quit()
 if __name__== "__main__":
   main()
 
