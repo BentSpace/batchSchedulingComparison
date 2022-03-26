d6ef0f298dc4c9f405240a7010a540f1c76e0d55
BentSpace <bentspacetime@gmail.com>
2022-03-26 13:16:31 -0700

    Added args

--- batchSchedulingComparison.py
+++ batchSchedulingComparison.py
@@ -1,5 +1,9 @@
+import sys
+
 def main():
   print("Hello World!")
+  print ('Number of arguments:', len(sys.argv), 'arguments.')
+  print ('Argument List:', str(sys.argv))
 
 if __name__== "__main__":
   main()
