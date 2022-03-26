3145162448ab0c84a5f47fb161c3898c4b7c44b5
BentSpace <bentspacetime@gmail.com>
2022-03-26 14:48:36 -0700

    File open, file name check added

--- batchSchedulingComparison.py
+++ batchSchedulingComparison.py
@@ -1,15 +1,28 @@
 import sys
+import os
 
 def main():
-  print("Hello World!")
-  print('Number of arguments:', len(sys.argv), 'arguments.')
-  print('Argument List:', str(sys.argv))
+  # Path to cwd
+  __location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    
+  # Check for 3 args
   if (len(sys.argv) != 3):
     print("\nMust use with 3 arguements. \n" +\
     "File name, batchfile name, and scheduling algorithm. \n" +\
     "Please try again.\n")
     quit()
+
+  batchFileName = sys.argv[1]
+  # Check whether file name exists
+  file_exists = os.path.exists(batchFileName)
+  if (not file_exists):
+    print("\nSorry that batch file does not exsist, please try again.\n")
+    quit()
+
+  # Open batch file
+  batchFile = open(os.path.join(__location__, batchFileName), 'r')
+  batchFileDataList = batchFile.readlines()
+  
 if __name__== "__main__":
   main()
-
-# print("Guru99")
